ca51e1a9b24f60903724e03eac9c77a5e1e9663b
Matt Porter <mtp5129@gmail.com>
2021-05-15 23:15:13 -0700

    working

--- scratch.cpp
+++ scratch.cpp
@@ -1,3 +1,8 @@
+/*
+main.cpp is the "good" code that does what we want. this file is just a scratch
+to mess around with.
+*/
+
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
@@ -5,6 +10,100 @@
 #include "opencv2/highgui.hpp"
 
 #include <iostream>
+
+cv::Mat do_sobel(cv::Mat in)
+{
+    cv::Mat img_grad_x;
+    cv::Mat img_grad_y;
+    cv::Sobel(in, img_grad_x, CV_64F, 1, 0);
+    cv::Sobel(in, img_grad_y, CV_64F, 0, 1);
+    cv::convertScaleAbs(img_grad_x, img_grad_x);
+    cv::convertScaleAbs(img_grad_y, img_grad_y);
+
+    cv::Mat out;
+    cv::addWeighted(img_grad_x, 0.33, img_grad_y, 0.33, 0, out);
+
+    return out;
+}
+
+void sobel_detection(cv::Mat img_original)
+{
+    cv::Mat img_blurred;
+    cv::Mat img_gray;
+    cv::GaussianBlur(img_original, img_blurred, cv::Size(9, 9), 0, 0, cv::BORDER_DEFAULT);
+    cv::cvtColor(img_blurred, img_gray, cv::COLOR_BGR2GRAY);
+
+    cv::imshow("sobol", do_sobel(img_original));
+}
+
+void canny_detection(cv::Mat img_original)
+{
+    cv::Mat img_blurred;
+    cv::Mat img_gray_blurred;
+    cv::Mat img_canny;
+
+    cv::GaussianBlur(img_original, img_blurred, cv::Size(9, 9), 0, 0, cv::BORDER_DEFAULT);
+    cv::cvtColor(img_blurred, img_gray_blurred, cv::COLOR_BGR2GRAY);
+    cv::Canny(img_gray_blurred, img_canny, 100, 200);
+
+    cv::imshow(__func__, img_canny);
+}
+
+void mser_detection(cv::Mat img_original)
+{
+    cv::Mat img_blurred;
+    cv::Mat img_output;
+
+    img_original.copyTo(img_output);
+
+    cv::GaussianBlur(img_original, img_blurred, cv::Size(99, 99), 0);
+
+    cv::Ptr<cv::MSER> mser = cv::MSER::create();
+    std::vector<std::vector<cv::Point>> regions;
+    std::vector<cv::Rect> bbox;
+    mser->detectRegions(img_blurred, regions, bbox);
+
+    for (int i = 0; i < regions.size(); i++)
+    {
+        cv::rectangle(img_output, bbox[i], CV_RGB(0, 255, 0));
+    }
+
+    cv::imshow(__func__, img_output);
+}
+
+void morph(cv::Mat img_original)
+{
+    cv::Mat img_gray;
+    cv::Mat img_out;
+
+    cv::cvtColor(img_original, img_out, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur(img_out, img_out, cv::Size(9, 9), 0);
+    img_out = do_sobel(img_out);
+    cv::morphologyEx(img_out, img_out, cv::MORPH_CLOSE, cv::Mat());
+
+    cv::imshow(__func__, img_out);
+}
+
+/*
+THRESH_TRIANGLE makes the image combine into a single solid which
+then makes it easier to determine the bounds of the image for cropping
+
+light mode = black area is what we want
+dark mode = gray area is what we want
+ */
+void threshold(cv::Mat src)
+{
+    cv::Mat dst;
+
+    src.copyTo(dst);
+
+    cv::cvtColor(dst, dst, cv::COLOR_BGR2GRAY);
+    cv::medianBlur(dst, dst, 5);
+    cv::threshold(dst, dst, 100, 200, cv::THRESH_TRIANGLE);
+    cv::morphologyEx(dst, dst, cv::MORPH_OPEN, cv::Mat());
+
+    cv::imshow(__func__, dst);
+}
 
 double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
 {
@@ -82,27 +181,27 @@ void max_square_edges(std::vector<std::vector<cv::Point>> src, std::vector<std::
             max_y = MAX(max_y, src[i][j].y);
             min_y = MIN(min_y, src[i][j].y);
 
-            std::cout
-                << "x: "
-                << src[i][j].x
-                << ", y: "
-                << src[i][j].y
-                << ", min x: "
-                << min_x
-                << ", max x: "
-                << max_x
-                << ", min y: "
-                << min_y
-                << ", max y: "
-                << max_y
-                << std::endl;
+            // std::cout
+            //     << "x: "
+            //     << src[i][j].x
+            //     << ", y: "
+            //     << src[i][j].y
+            //     << ", min x: "
+            //     << min_x
+            //     << ", max x: "
+            //     << max_x
+            //     << ", min y: "
+            //     << min_y
+            //     << ", max y: "
+            //     << max_y
+            //     << std::endl;
         }
 
-        std::cout << std::endl;
-        std::cout << "max x: " << max_x << std::endl;
-        std::cout << "min x: " << min_x << std::endl;
-        std::cout << "max y: " << max_y << std::endl;
-        std::cout << "min y: " << min_y << std::endl;
+        // std::cout << std::endl;
+        // std::cout << "max x: " << max_x << std::endl;
+        // std::cout << "min x: " << min_x << std::endl;
+        // std::cout << "max y: " << max_y << std::endl;
+        // std::cout << "min y: " << min_y << std::endl;
 
         std::vector<cv::Point> square_p;
 
@@ -154,28 +253,28 @@ bool first_row_is_white(cv::Mat src)
 
     int avg = total / len;
 
-    std::cout
-        << "Average: "
-        << avg
-        << std::endl;
+    // std::cout
+    //     << "Average: "
+    //     << avg
+    //     << std::endl;
 
     int d_white = abs(255 - avg);
     int d_black = abs(0 - avg);
     bool is_white = d_white < d_black;
 
-    std::cout
-        << "is white: "
-        << is_white
-        << ", d_white: "
-        << d_white
-        << ", d_black: "
-        << d_black
-        << std::endl;
+    // std::cout
+    //     << "is white: "
+    //     << is_white
+    //     << ", d_white: "
+    //     << d_white
+    //     << ", d_black: "
+    //     << d_black
+    //     << std::endl;
 
     return is_white;
 }
 
-void find_image(cv::Mat src)
+void threshold_contours(cv::Mat src)
 {
     cv::Mat dst;
 
@@ -203,7 +302,7 @@ void find_image(cv::Mat src)
 
     // cvtColor to bgr so that polylines are green and not gray
     cv::cvtColor(dst, dst, cv::COLOR_GRAY2BGR);
-    cv::polylines(dst, squares, true, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+    cv::polylines(dst, squares, true, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
 
     cv::imshow(__func__, dst);
 }
@@ -219,7 +318,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    find_image(img);
+    // canny_detection(img);
+    // mser_detection(img); // rule out mser for now
+    // sobel_detection(img);
+    // morph(img);
+    // threshold(img);
+    threshold_contours(img);
 
     char key;
     do
